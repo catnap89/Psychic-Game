ae49b5cf721b78f76a8c83370c61777866a172ff
youngwoo.cho <petshop0907@gmail.com>
2019-06-21 11:05:37 -0400

    fully functioning now with new feature that shows the lists of user guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,16 @@
 
 PSUEDOCODING
 
-1. Wins start as 0
-2. Loses start as 0
-3. Guesses Left start as 9
+1. Wins start as 0 (Done)
+2. Loses start as 0 (Done)
+3. Guesses Left start as 9 (Done)
 
-4. User presses letter key to start the game. --> make all key input lowercase
-   The key pressed is displayed in Your Guesses So Far:
-5. Computer choice randomly from A~Z when the game is started
-6. if user input === computer guess, win increase by one --> reset computer choice
-7. else, guesses left decreases by 1 until it reaches -1. 
-8. When guesses left reaches -1, it resets to 9 and Loses increase by 1. --> reset computer choice
+4. User presses letter key to start the game. --> make all key input lowercase (Done)
+   The key pressed is displayed in Your Guesses So Far: (Done)
+5. Computer choice randomly from A~Z when the game is started (done)
+6. if user input === computer guess, win increase by one --> reset computer choice (done)
+7. else, guesses left decreases by 1 until it reaches -1. (done)
+8. When guesses left reaches -1, it resets to 9 and Loses increase by 1. --> reset computer choice (done)
 
 
 */
@@ -28,51 +28,52 @@ var guessCounter = 9;
 // Array of the alphabets that computer can guess from
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var guessArr = [];  // this array will be used to store the user inputs in userInput paragraph.
 
-// Randomly chooses from 
+// Randomly chooses from letters variable
+// Not sure why this variable alone is generating random letters without a function to run it?
 var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 console.log(computerChoice);
-
-
 
 // Functions
 // =======================================================================
 
-document.onkeyup = function(event) {
-  var userInput = event.key;
-  var lowerCase = userInput.toLocaleLowerCase(); 
+  document.onkeyup = function(event) {
+    var userInput = event.key;
+    var lowerCase = userInput.toLocaleLowerCase(); 
+    guessArr.push(lowerCase);
 
-  // Display userinput in Your Guesses So Far.
+    if (lowerCase === computerChoice) {
 
-  if (lowerCase === computerChoice) {
-    winCounter++;
-    // Reset computerChoice
-    // Reset guessCounter
-    resetGame();
-  } else {
-    guessCounter--;
-  }
+      winCounter++;
+      resetGame();  // Resets computerChoice and guessCounter
 
-  if (guessCounter < 0) {
-    lossCounter++;
-    // Reset computerChoice
-    // Reset guessCounter
-    resetGame();
-  }
+    } else {
 
+      guessCounter--;
 
-  document.getElementById("wins").innerHTML = "Wins: " + winCounter;
-  document.getElementById("loses").innerHTML = "Loses: " + lossCounter;
-  document.getElementById("guesses").innerHTML = "Guesses Left: " + guessCounter;
-  
-}
+    };
+
+    if (guessCounter < 0) {
+
+      lossCounter++;
+      resetGame();
+
+    };
 
 
-function resetGame() {
-  guessCounter = 9;
+    document.getElementById("wins").innerHTML = "Wins: " + winCounter;
+    document.getElementById("loses").innerHTML = "Loses: " + lossCounter;
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessCounter;
+    document.getElementById("userInput").innerHTML = "Your Guesses So Far: " + guessArr;
+   
+  };
 
-  computerChoice = letters[Math.floor(Math.random() * letters.length)];
-  console.log(computerChoice);
-}
 
+  function resetGame() {
+    guessCounter = 9;
+
+    computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    console.log(computerChoice);
+  };
 
